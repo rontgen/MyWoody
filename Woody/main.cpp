477d145012e21f664b746ae1016719c5eea45446
gwang <minstrelgen@163.com>
2021-06-13 21:58:02 +0800

    add SDL basic framework

---
+++ Woody/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "SDL.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) //main函数必须要有参数
+{
+	SDL_Window *pWindow = NULL;
+	SDL_Init(SDL_INIT_VIDEO);
+
+	pWindow = SDL_CreateWindow("SDL2 Window", 100, 100, 640, 480, SDL_WINDOW_SHOWN /*| SDL_WINDOW_BORDERLESS*/);
+	if (!pWindow)
+	{
+		cout << "window create failed" << endl;
+		SDL_Quit();
+		return -1;
+	}
+
+	cout << "Window Create Success" << endl;
+	SDL_DestroyWindow(pWindow);
+
+	SDL_Quit();
+	return 0;
+}
